009899803d668357ba75d008e0f05cf468c8b6b7
Rory McCann <rory@technomancy.org>
2014-08-20 21:25:03 +0100

    files

---
+++ setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='gedcom-rdf',
+    version='0.1',
+    py_modules=['gedcomrdf',],
+    license='GPLv3+',
+    test_suite='tests',
+    description="Convert GEDCOM files to/from RDF",
+    author="Rory McCann",
+    author_email="rory@technomancy.org",
+    classifiers=[
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Topic :: Sociology :: Genealogy',
+        'Topic :: Sociology :: History',
+        'Topic :: Text Processing',
+      ],
+)
