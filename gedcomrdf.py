4685857b10422b23b327e065f09053832582547a
Rory McCann <rory@technomancy.org>
2014-08-21 21:30:55 +0100

    work on forward and backward testing

--- gedcomrdf.py
+++ gedcomrdf.py
@@ -1,11 +1,23 @@
+import sys
+
 import rdflib
 import gedcom
+from gedcom import Individual
 from rdflib.namespace import DC, FOAF, Namespace
 from rdflib import RDF, Literal, BNode
 
 
-def gedcom2rdf(gedcom_filename, rdf_filename):
+def gedcom2rdf_files(gedcom_filename, rdf_filename):
     gedcomfile = gedcom.parse_filename(gedcom_filename)
+
+    output_graph = gedcom2rdf(gedcomfile)
+
+
+    with open(rdf_filename, 'w') as fp:
+        fp.write(output_graph.serialize(format="turtle"))
+
+def gedcom2rdf(gedcomfile):
+
     gedcom_individuals = gedcomfile.individuals
 
     output_graph = rdflib.Graph()
@@ -109,15 +121,85 @@ def gedcom2rdf(gedcom_filename, rdf_filename):
                 output_graph.add( (marriage_node, bio.place, Literal(family['MARR'].place)) )
             except IndexError:
                 pass
-                                
 
-            
-        
+    return output_graph
 
+class UnconvertableRDFGraph(Exception):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        self.kwargs = kwargs
 
-    with open(rdf_filename, 'w') as fp:
-        fp.write(output_graph.serialize(format="turtle"))
+    def __str__(self):
+        return "{}({})".format(self.__class__.__name__, repr(self.kwargs))
 
+class NonGedcomRecongisedSex(UnconvertableRDFGraph):
+    pass
 
-gedcom2rdf('BUELL001.GED', 'buell.ttl')
-#gedcom2rdf('sample.ged', 'sample.ttl')
+def rdf2gedcom(rdf_graph):
+    gedcomfile = gedcom.GedcomFile()
+    # all individuals
+    people = rdf_graph.query("""
+        SELECT ?personuri ?firstname ?lastname ?gender
+        WHERE {
+            ?personuri a foaf:Person
+            OPTIONAL { ?personuri foaf:givenName ?firstname }
+            OPTIONAL { ?personuri foaf:familyName ?lastname }
+            OPTIONAL { ?personuri foaf:gender ?gender }
+        }""")
+
+    personuri_to_gedcom = {}
+
+    for person in people:
+        uri, firstname, lastname, gender = person
+            # use .value to turn rdf Literals in strings
+        firstname = firstname.value
+        lastname = lastname.value
+        gender = gender.value
+        individual = gedcomfile.individual()
+        if firstname or lastname:
+            name = gedcomfile.element("NAME")
+            individual.add_child_element(name)
+            if firstname:
+                name.add_child_element(gedcomfile.element("GIVN", value=firstname))
+            if lastname:
+                name.add_child_element(gedcomfile.element("SURN", value=lastname))
+        if gender:
+            if gender == 'female':
+                gender = 'F'
+            elif gender == 'male':
+                gender = 'M'
+            else:
+                raise NonGedcomRecongisedSex(rdfsex=gender)
+            individual.add_child_element(gedcomfile.element('SEX', value=gender))
+
+        gedcomfile.add_element(individual)
+        personuri_to_gedcom[uri] = individual
+
+    # try to figure out families
+    # Simple families, 2 people
+    # look for a marriage event between 2 people of different genders.
+    marriages = rdf_graph.query("""
+        SELECT ?marriageuri ?malepartneruri ?femalepartneruri ?date ?place
+        WHERE {
+            ?marriageuri a bio:Marriage ;
+                bio:partner ?malepartneruri , ?femalepartneruri .
+            ?malepartneruri a foaf:Person ; foaf:gender 'male' .
+            ?femalepartneruri a foaf:Person ; foaf:gender 'female' .
+            OPTIONAL { ?marriageuri bio:date ?date . }
+            OPTIONAL { ?marriageuri bio:place ?place . }
+        }""")
+
+    for marriage in marriages:
+        marriageuri, malepartneruri, femalepartneruri, date, place = marriage
+        #date, place = date.value if date else N, place.value
+
+        family = gedcomfile.family()
+        family.add_child_element(gedcomfile.element("HUSB", value=personuri_to_gedcom[malepartneruri].id))
+        family.add_child_element(gedcom.(value=personuri_to_gedcom[femalepartneruri].id))
+
+        gedcomfile.add_element(family)
+
+    return gedcomfile
+
+if __name__ == '__main__':
+    gedcom2rdf_files(sys.argv[1], sys.argv[2])
